80864e33d2959e3ecc1a10a4232fc4f81d4c4333
Cyrus <cyrus.lee@imobilebi.com>
2023-12-23 21:32:17 +0800

    build: project init

---
+++ src/main/java/com/pipi/satrestfulapi/SatRestfulapiApplication.java
@@ -0,0 +1,13 @@
+package com.pipi.satrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatRestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatRestfulapiApplication.class, args);
+	}
+
+}
